dbe40d64956b2ea513f4c9497f4a4038a7c5f17e
HieuNT1998 <trihieua2@gmail.com>
2020-06-06 23:27:58 +0700

    database

--- router/apiUser.js
+++ router/apiUser.js
@@ -2,7 +2,8 @@ const express = require('express');
 const apiUser = express.Router();
 var {Pool,Client} = require("pg");
   
-var connectionString = "postgres://postgres:123@localhost:5432/BattleShip";
+// var connectionString = "postgres://postgres:123@localhost:5432/BattleShip";
+var connectionString = "postgres://xomkroolbhzapo:3e72b69723095a2444742e9eb2e51eaaed37c12bdcd1d06e1e36790277b6809a@ec2-34-194-198-176.compute-1.amazonaws.com:5432/d1k28m2g68cauf"; 
 var pool = new Pool({
   connectionString:  connectionString,
 })
