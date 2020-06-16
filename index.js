b4f2aa2262a9ec2ae4b3422f177eb277475d967c
HieuNT1998 <trihieua2@gmail.com>
2020-06-16 22:37:38 +0700

    socket io maching

--- index.js
+++ index.js
@@ -2,28 +2,137 @@ const express = require('express');
 const app = express();
 const bodyParse = require('body-parser')
 const AuthRouter = require('./router/apiAuth');
-const UserRouter = require('./router/apiUser')
-
+const UserRouter = require('./router/apiUser');
+const { use } = require('./router/apiAuth');
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.use(bodyParse.urlencoded({ extended: false }));
 app.use(bodyParse.json());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
-    next();
+	res.header("Access-Control-Allow-Origin", '*');
+	res.header('Access-Control-Allow-Credentials', 'true')
+	res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
+	next();
 });
 
-app.get('/',(req,res)=>{
-    res.send("<h1>HELLO ANH EM </h1>")
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + "/tests.html")
 })
 
-app.use("/auth",AuthRouter);
-app.use("/users",UserRouter);
+app.use("/auth", AuthRouter);
+app.use("/users", UserRouter);
 
-app.listen(process.env.PORT || 6969, (err) => {
-    if (err) console.log(err);
-    else console.log("Server Ready on port : 6969");
+//++++++++++++++++++++++++++++++++++++++++++++ Socket IO Handle +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+var user_online = []
+// status 1: ready to fight
+// status 2: wait confirm 
+// status 3: wait matching radom
+// status 4: in game
+
+function get_index_by_socketId(socketId){
+	for(let i=0;i<user_online.length(); i++){
+		if(user_online[i].socketId == socketId) return i
+	}
+	return -1
+}
+
+
+io.on('connection', (socket) => {
+	console.log("co nguoi ket noi: " + socket.id)
+
+	socket.on('c2s_online_alert', (data) => {
+		user = {
+			socketId: socket.id,
+			infor: data,
+			staus: 1
+		}
+		user_online.push(user_online)
+	})
+
+	socket.on('c2s_online_list', () => {
+		socket.emit('s2c_online_list', user_online)
+	})
+
+	socket.on('c2s_thach_dau', (data) => {
+		var id = '123456789'                         // get socketid from data
+		
+		var host_index = get_index_by_socketId(id)   // set status user		
+		var guest_index = get_index_by_socketId(socket.id)  // handle request
+
+		if (guest_index = -1) {
+			let message = {
+				success: 0,
+				content: "User was offline"
+			}
+			socket.emit('s2c_thach_dau', message)
+		}
+		else if (user_online[index].status != 1) {
+			let message = {
+				success: 0,
+				content: "User not ready"
+			}
+			socket.emit('s2c_thach_dau', message)
+		}
+		else{
+			let message = {
+				succcess : 1,
+				host_socketId: user_online[host_index].socketId,
+				guest_socketId: user_online[guest_index].socketId, 
+			}
+			user_online[host_index].status = 2
+			socket.emit('s2c_thach_dau',message)
+			io.to(user_online[guest_index].socketId).emit('s2c_loi_moi', message)
+		}
+	})
+
+	socket.on('c2s_huy_moi',(data)=>{
+		index = get_index_by_socketId(socket.id)
+		user_online[index].status = 1
+	})
+
+	socket.on('c2s_phan_hoi', (data) => {
+		var status = true;                            // get status from data
+		var id = '1243456';														// get socketId host from data
+
+		var guest_index = get_index_by_socketId(socket.id)
+		var host_index = get_index_by_socketId(id)
+
+		if (status) {                                       // confirm 
+			if (user_online[host_index].status != 2) {        // host not wait
+				let message = {
+					success: 0,
+					content: "host is not wait"
+				}
+				socket.emit('s2c_phan_hoi', message)
+			}
+			else {
+				let message = {
+					success: 1,
+					host_socketId : user_online[host_index].socketId,
+					guest_socketId : user_online[guest_index].socketId
+				}
+				user_online[guest_index].status = 4
+				user_online[host_index].status = 4 
+				io.to(user_online[host_index].socketId).emit('s2c_chap_nhan', message)
+				socket.emit("s2c_phan_hoi",message)
+			}
+		}                                                      
+		else if (user_online[host_index].status == 2) {        // not confirm - host still wait
+			let message = {
+				success: 0,
+				content: "guest is not confirm"
+			}
+			user_online[host_index].status = 1
+			io.to(user_online[host_index].socketId).emit('s2c_chap_nhan', message)
+		}
+	})
+})
+
+http.listen(process.env.PORT || 5000, (err) => {
+	if (err) console.log(err);
+	else console.log("Server Ready on port : 5000");
 })
--- router/apiUser.js
+++ router/apiUser.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const apiUser = express.Router();
-var {Pool,Client} = require("pg");
-  
+var { Pool, Client } = require("pg");
+
 // var connectionString = "postgres://postgres:123@localhost:5432/BattleShip";
-var connectionString = "postgres://xomkroolbhzapo:3e72b69723095a2444742e9eb2e51eaaed37c12bdcd1d06e1e36790277b6809a@ec2-34-194-198-176.compute-1.amazonaws.com:5432/d1k28m2g68cauf"; 
+var connectionString = "postgres://xomkroolbhzapo:3e72b69723095a2444742e9eb2e51eaaed37c12bdcd1d06e1e36790277b6809a@ec2-34-194-198-176.compute-1.amazonaws.com:5432/d1k28m2g68cauf";
 var pool = new Pool({
-  connectionString:  connectionString,
+  connectionString: connectionString,
 })
 
 // create new user
-apiUser.post("/",(req,res)=>{
-  var {email, password, name, avatarUrl} = req.body;
-  if(!avatarUrl) avatarUrl = "https://image.flaticon.com/icons/svg/2919/2919600.svg";
+apiUser.post("/", (req, res) => {
+  var { email, password, name, avatarUrl } = req.body;
+  if (!avatarUrl) avatarUrl = "https://image.flaticon.com/icons/svg/2919/2919600.svg";
   pool.query(`insert into USERS (EMAIL, PASSWORD, NAME, AVARTAR_URL)  values ('${email}', ${password}, '${name}', '${avatarUrl}')`,
-    (err,data)=>{
-      if(err) res.status(500).send({success:0,err})
-      else{
-        res.status(201).send({success:1,userName : data.rows[0]})
+    (err, data) => {
+      if (err) res.status(500).send({ success: 0, err })
+      else {
+        res.status(201).send({ success: 1, userName: data.rows[0] })
       }
     })
 })
@@ -24,10 +24,10 @@ apiUser.post("/",(req,res)=>{
 // get all user
 apiUser.get('/', (req, res) => {
   pool.query(`select * from USERS`,
-    (err,data)=>{
-      if(err) res.status(500).send({success:0,err})
-      else{
-        res.status(201).send({success:1,userName : data.rows})
+    (err, data) => {
+      if (err) res.status(500).send({ success: 0, err })
+      else {
+        res.status(201).send({ success: 1, userName: data.rows })
       }
     })
 });
@@ -36,39 +36,52 @@ apiUser.get('/', (req, res) => {
 apiUser.get('/:id', (req, res) => {
   id = req.params.id
   pool.query(`select * from USERS WHERE ID = ${id}`,
-  (err,data)=>{
-    if(err) res.status(500).send({success:0,err})
-    else{
-      res.status(201).send({success:1,userName : data.rows})
-    }
-  })
+    (err, data) => {
+      if (err) res.status(500).send({ success: 0, err })
+      else {
+        res.status(201).send({ success: 1, userName: data.rows })
+      }
+    })
 });
 
 // apdate user by id
 apiUser.put('/:id', (req, res) => {
   console.log("update infor")
   id = req.params.id;
-  var {password, name, avartar_url} = req.body
-  pool.query(`update users 
+  var { password, name, avartar_url } = req.body
+  if (name == null && avartar_url == null) {
+    pool.query(`update users 
+              set password = '${password}'
+              where id = ${id}
+              `, (err, data) => {
+      if (err) res.status(500).send({ success: 0, err })
+      else {
+        res.status(201).send({ success: 1, message: "success" })
+      }
+    })
+  }
+  else {
+    pool.query(`update users 
               set password = '${password}', name = '${name}', avartar_url = '${avartar_url}'
               where id = ${id}
-              `,(err,data)=>{
-                if(err) res.status(500).send({success:0, err})
-                else{
-                  res.status(201).send({success:1,message:"success"})
-                }
-              }) 
+              `, (err, data) => {
+      if (err) res.status(500).send({ success: 0, err })
+      else {
+        res.status(201).send({ success: 1, message: "success" })
+      }
+    })
+  }
 })
 
 // delete user by id
-apiUser.delete('/:id',(req,res)=>{
+apiUser.delete('/:id', (req, res) => {
   console.log("delete user by id")
   id = req.params.id;
   pool.query(`delete from users 
-              where id = ${id}`,(err,data)=>{
-                if(!err) res.status(201).send({success:1, message:"success"})
-                else res.status(500).send({success:0, message:"delete fail"})
-              })
+              where id = ${id}`, (err, data) => {
+    if (!err) res.status(201).send({ success: 1, message: "success" })
+    else res.status(500).send({ success: 0, message: "delete fail" })
+  })
 
 })
 
